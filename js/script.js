68d1a99da8bd7e2b578e3b314219807db9b0016a
Hira Babu <50524208+monwar906810@users.noreply.github.com>
2023-09-07 02:06:29 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*=========================Typing Animation========================= */
+var typed = new Typed(".porfession",{
+    strings:["","Web Designer","Web Developer","PHP Developer","Laravel Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
